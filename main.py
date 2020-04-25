d5c2ca96b62b5a4fb9372993cc1c6ca007345c6d
smit2k14 <smir2k14@gmail.com>
2020-04-25 16:39:32 +0530

    I am a f**king idiot

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ N_LINES = [4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
 N_FEATURES = [4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
 print "Making serial outfile file"
 os.system('touch output_file_serial.txt')
-    
+ 
 with open('output_file_serial.txt', 'w') as f:
     f.write("")
 
 print "Will start serial code now: "
 for n_lines in N_LINES:
     for n_features in N_FEATURES:
+        
+        with open('output_file_serial.txt', 'a') as f:    
+            f.write('\n\nNumber of lines: ' + str(n_lines) + ' Number of features: ' + str(n_features)+'\n\n')
         print "Generating features for: " + str(n_features) + " and lines: " + str(n_lines)
         os.system('python generate_data.py ' + str(n_lines) + ' ' + str(n_features))
         print "Compiling serial code: "
@@ -26,15 +29,13 @@ os.system('touch output_file_parallel.txt')
 with open('output_file_parallel.txt', 'w') as f:
     f.write("")
 
-for p in range(1, 3):
-    
+for p in range(1, 16):
     for n_lines in N_LINES:
         for n_features in N_FEATURES:
-            
             with open("output_file_parallel.txt", "a") as f:
                 f.write("Processors: "+str(p)+" N_LINES: "+str(n_lines)+" N_FEATURES: "+str(n_features)+"\n\n")
             print "Generating features for: " + str(n_features) + " and lines: " + str(N_LINES) + " and processors: " + str(p)
-            os.system('python generate_data.py '+str(N_LINES)+' '+str(n_features))
+            os.system('python generate_data.py '+str(n_lines)+' '+str(n_features))
             print "Compiling parallel code: "
             os.system('g++ -std=c++11 parallel_measurement.cpp -fopenmp -o parallel')
             print "Running parallel code: "
