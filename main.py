3a2526f2e9a189c2bf8f7f21eb475d9d86cfe246
Kartikeya Gokhale <kgkartikeyagokhale@gmail.com>
2020-05-03 13:39:06 +0530

    Took compiling out of loops. Added better debug statements

--- main.py
+++ main.py
@@ -10,14 +10,20 @@ os.system('touch output_file_serial.txt')
 with open('output_file_serial.txt', 'w') as f:
     f.write("")
 
-print "Will start serial code now: "
+print "Compiling serial code =>"
+os.system('gcc serial_measurement.c -fopenmp -lm -o serial')
+
+print "Running serial code =>"
 for n_lines in N_LINES:        
     with open('output_file_serial.txt', 'a') as f:    
         f.write('\n\nNumber of lines: ' + str(n_lines) + ' Number of features: ' + str(n_lines)+'\n\n')
-    print "Compiling serial code for N_Lines: " + str(n_lines) + " N_Features: " + str(n_lines)
-    os.system('gcc serial_measurement.c -fopenmp -lm -o serial')
-    print "Running serial code: "
+    print "Running serial code for N_Lines: " + str(n_lines) + " N_Features: " + str(n_lines)
     os.system('./serial '+str(n_lines)+' '+str(n_lines))
+    print("-------------------------------")
+
+print "==================================="
+print "Serial done. Will do parallel now!"
+print "==================================="
 
 print "Making parallel output file"
 os.system('touch output_file_parallel.txt')
@@ -25,13 +31,15 @@ os.system('touch output_file_parallel.txt')
 with open('output_file_parallel.txt', 'w') as f:
     f.write("")
 
-for p in range(1, 5):
+print "Compiling parallel code =>"
+os.system('gcc parallel_measurement.c -fopenmp -lm -o parallel')
+
+print "Running parallel code now =>"
+for p in range(1, 17):
     for n_lines in N_LINES:
         with open("output_file_parallel.txt", "a") as f:
             f.write("Processors: "+str(p)+" N_LINES: "+str(n_lines)+" N_FEATURES: "+str(n_lines)+"\n\n")
-        print "Compiling parallel code: for N_Processors: " + str(p) + " N_Lines: " + str(n_lines) + " N_Features: " + str(n_lines)
-        os.system('gcc parallel_measurement.c -fopenmp -lm -o parallel')
-        print "Running parallel code: "
+        print "Running parallel code: for N_Processors: " + str(p) + " N_Lines: " + str(n_lines) + " N_Features: " + str(n_lines)
         os.system('./parallel '+str(p)+' '+str(n_lines)+' '+str(n_lines))
     with open('output_file_parallel.txt', 'a') as f:
         f.write('\n\n')
